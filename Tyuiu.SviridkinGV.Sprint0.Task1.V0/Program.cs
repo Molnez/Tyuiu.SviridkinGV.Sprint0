60dc45a0a4a44b0ab769adb99923965754b5cca2
Molnez <gleblongyugan@gmail.com>
2026-09-19 20:11:23 +0500

    Task1

---
+++ Tyuiu.SviridkinGV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.SviridkinGV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Глеб ";
+            int age = 17;
+            Console.WriteLine("Здраствуйте меня зовут " + name + "мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
