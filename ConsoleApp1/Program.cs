1a6f0b0c9d296d9ad6e270847ea99ace75fe1b6d
Molnez <gleblongyugan@gmail.com>
2026-09-19 20:20:00 +0500

    Task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Глеб ";
+            int age = 17;
+            Console.WriteLine("Здраствуйте меня зовут " + name + "мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
